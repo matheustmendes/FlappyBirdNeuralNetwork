b6e7c239d592d510741b51b297d57ce9fa5a1d8e
matheus <matios.torrres@gmail.com>
2025-06-18 17:57:01 -0300

    initializing the neural network with the classes layer.cs and neuron.cs

--- FlappyBirdNNetwork/Layer.cs
+++ FlappyBirdNNetwork/Layer.cs
@@ -2,5 +2,21 @@
 
 public class Layer
 {
-    
+    public Neuron[] Neurons;
+
+    public Layer(int neuronCount, int inputCountPerNeuron)
+    {
+        Neurons = new Neuron[neuronCount];
+        for(int i = 0; i < neuronCount; i++)
+        {
+            Neurons[i] = new Neuron(inputCountPerNeuron);
+        }
+
+        double[] FeedForward(double[] inputs)
+        {
+            return Neurons.Select(n => n.Activate(inputs)).ToArray();
+        }
+        
+        
+    }
 }
