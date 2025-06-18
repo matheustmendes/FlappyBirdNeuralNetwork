ef9185e8d62962e40a62d75fa64ac1d643432770
matheus <matios.torrres@gmail.com>
2025-06-18 18:17:28 -0300

    added Mutate and Crossover methods for Genetic algorithm

--- FlappyBirdNNetwork/NeuralNetwork.cs
+++ FlappyBirdNNetwork/NeuralNetwork.cs
@@ -28,4 +28,62 @@ public class NeuralNetwork
         }
         return output;
     }
+
+    public void Mutate(double mutationRate)
+    {
+        var rand = new Random();
+
+        foreach (var layer in Layers)
+        {
+            foreach (var neuron in layer.Neurons)
+            {
+                for (int i = 0; i < neuron.Weights.Length; i++)
+                {
+                    if(rand.NextDouble() < mutationRate)
+                    {
+                        // Mutate weight
+                        neuron.Weights[i] += (rand.NextDouble() * 2 - 1) * 0.1; // pequena mutação
+                        
+                    }
+                }
+
+                if (rand.NextDouble() < mutationRate)
+                {
+                    neuron.Bias += (rand.NextDouble() * 2 - 1) * 0.1;
+                }
+            }
+        }
+    }
+
+    public NeuralNetwork Crossover(NeuralNetwork partner)
+    {
+        var child = new NeuralNetwork(
+            Layers[0].Neurons[0].Weights.Length,
+            Layers.Take(Layers.Length - 1).Select(l => l.Neurons.Length).ToArray(), 
+            Layers.Last().Neurons.Length);
+        
+        var rand = new Random();
+
+        for (int l = 0; l < Layers.Length; l++)
+        {
+            for (int n = 0; n < Layers[l].Neurons.Length; n++)
+            {
+                var neuron = Layers[l].Neurons[n];
+                var partnerNeuron = partner.Layers[l].Neurons[n];
+                var childNeuron = child.Layers[l].Neurons[n];
+
+
+                for (int w = 0; w < neuron.Weights.Length; w++)
+                {
+                    childNeuron.Weights[w] = rand.NextDouble() < 0.5 
+                        ? neuron.Weights[w] 
+                        : partnerNeuron.Weights[w];
+                }
+                
+                childNeuron.Bias = rand.NextDouble() < 0.5 ? neuron.Bias : partnerNeuron.Bias;
+            }
+        }
+
+        return child;
+    }
 }
